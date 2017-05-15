6ca8a25201ddd0c1c2c15fd64a065444f32c9be3
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-15 19:19:13 +0100

    create methods to recognise item categories

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,7 +8,19 @@ class GildedRose
   end
 
   def is_normal?(item)
-    ! ["Aged Brie", "Backstage passes", "Sulfuras"].include?(item.name)
+    ! ['Aged Brie', 'Backstage passes', 'Sulfuras'].include?(item.name)
+  end
+
+  def is_aged_brie?(item)
+    item.name == 'Aged Brie'
+  end
+
+  def is_backstage_passes?(item)
+    item.name == 'Aged Brie'
+  end
+
+  def is_sulfuras?(item)
+    item.name == 'Sulfuras'
   end
 
   def reduce_quality(item)
@@ -28,19 +40,18 @@ class GildedRose
       if is_normal?(item)
         reduce_quality(item)
       else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == "Backstage passes"
-            if item.sell_in < 11
-              increase_quality(item)
-            end
-            if item.sell_in < 6
-              increase_quality(item)
-            end
+        increase_quality(item)
+        if item.name == "Backstage passes"
+          if item.sell_in < 11
+            increase_quality(item)
+          end
+          if item.sell_in < 6
+            increase_quality(item)
           end
         end
       end
-      if item.name != "Sulfuras"
+
+      if ! is_sulfuras?(item)
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
