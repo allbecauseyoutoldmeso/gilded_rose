3d744e20cf62ac3c9ef7ffff8066a50b6c27173c
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-15 16:48:44 +0100

    more tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,7 +2,7 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  subject(:gilded_rose) { described_class.new([Item.new('Malbec', 10, 10)])}
+  subject(:gilded_rose) { described_class.new([Item.new('Malbec', 5, 10)])}
 
   describe "#update_quality" do
 
@@ -18,7 +18,21 @@ describe GildedRose do
 
     it 'reduces the sell_in time by one' do
       gilded_rose.update_quality
-      expect(gilded_rose.items[0].sell_in).to eq 9
+      expect(gilded_rose.items[0].sell_in).to eq 4
+    end
+
+    it 'reduces quality by 2 when sell_by date has passed' do
+      6.times do
+        gilded_rose.update_quality
+      end
+      expect(gilded_rose.items[0].quality).to eq 3
+    end
+
+    it 'never reduces quality to less than zero' do
+      10.times do
+        gilded_rose.update_quality
+      end
+      expect(gilded_rose.items[0].quality).to eq 0
     end
 
   end
