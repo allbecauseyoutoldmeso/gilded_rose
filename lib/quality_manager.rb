b7a6927d56a142f76e2c52ac54a354d03862e2a9
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-16 10:26:20 +0100

    extract methods to quality manager class

---
+++ lib/quality_manager.rb
@@ -0,0 +1,79 @@
+class Quality_Manager
+
+  attr_reader :items
+
+  def initialize(items)
+    @items = items
+  end
+
+  def is_normal?(item)
+    ! ['Aged Brie', 'Backstage passes', 'Sulfuras', 'Conjured'].include?(item.name)
+  end
+
+  def is_aged_brie?(item)
+    item.name == 'Aged Brie'
+  end
+
+  def is_backstage_pass?(item)
+    item.name == 'Backstage passes'
+  end
+
+  def is_sulfuras?(item)
+    item.name == 'Sulfuras'
+  end
+
+  def is_conjured?(item)
+    item.name == 'Conjured'
+  end
+
+  def reduce_quality(item)
+    if item.quality > 0
+      item.quality -= 1
+    end
+  end
+
+  def increase_quality(item)
+    if item.quality < 50
+      item.quality += 1
+    end
+  end
+
+  def update_normal_quality(item)
+    item.sell_in > 0 ? reduce_quality(item) : 2.times { reduce_quality(item) }
+  end
+
+  def update_aged_brie_quality(item)
+    increase_quality(item)
+  end
+
+  def update_backstage_pass_quality(item)
+    if item.sell_in <= 0
+      item.quality = 0
+    elsif item.sell_in <=5
+      3.times { increase_quality(item) }
+    elsif item.sell_in <= 10
+      2.times { increase_quality(item) }
+    elsif
+      increase_quality(item)
+    end
+  end
+
+  def update_conjured_quality(item)
+    item.sell_in > 0 ? 2.times { reduce_quality(item) } : 4.times { reduce_quality(item) }
+  end
+
+  def update
+    items.each do |item|
+      if is_normal?(item)
+        update_normal_quality(item)
+      elsif is_aged_brie?(item)
+        update_aged_brie_quality(item)
+      elsif is_backstage_pass?(item)
+        update_backstage_pass_quality(item)
+      elsif is_conjured?(item)
+        update_conjured_quality(item)
+      end
+    end
+  end
+
+end
--- lib/value_manager.rb
+++
@@ -1,2 +0,0 @@
-class Value_Manager
-end 
