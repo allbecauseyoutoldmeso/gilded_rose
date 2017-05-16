feb5932a028283a5f18162d2995f6e43916126f1
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-16 12:15:39 +0100

    set up golden master test

---
+++ spec/golden_master_spec.rb
@@ -0,0 +1,19 @@
+golden_master = File.read('golden_master.txt')
+golden_master_lines = golden_master.split("\n")
+
+# gilded_rose_file = 'gilded_rose.rb'
+
+current_output = `ruby texttest_fixture.rb 1 'gilded_rose.rb'`
+current_output_lines = current_output.split("\n")
+
+# require 'pry'; binding.pry
+
+lines_to_compare = [golden_master_lines.count, current_output_lines.count].max
+
+describe 'Golden Master for GildedRose' do
+  lines_to_compare.times do |line_num|
+    it "match line #{line_num}: #{current_output_lines[line_num]} should equal #{golden_master_lines[line_num]}" do
+      expect(current_output_lines[line_num]).to eq golden_master_lines[line_num]
+    end
+  end
+end
